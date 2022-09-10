67bd48bc0396fdccc8372d9f1dad71ab4927065e
Endreu Benites <endreu.matteusrb@gmail.com>
2022-09-09 21:49:57 -0300

    Estilizando componente Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,9 +5,9 @@ function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img 
-                        className={styles.avatar} 
-                        src="https://github.com/endreumrb.png" 
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/endreumrb.png"
                     />
                     <div className={styles.authorInfo}>
                         <strong>Endreu Benites</strong>
@@ -23,9 +23,13 @@ function Post() {
 
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-                <p>👉 <a href='#'>jane.design/doctorcare</a></p>
+                <p><a href='#'>jane.design/doctorcare</a></p>
 
-                <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
+                <p>
+                    <a href='#'>#novoprojeto</a>{' '}
+                    <a href='#'>#nlw</a>{' '}
+                    <a href='#'>#rocketseat</a>
+                </p>
             </div>
         </article>
     );

9b552a21da249916a283838afa3898ffb88fc16e
Endreu Benites <endreu.matteusrb@gmail.com>
2022-09-14 14:35:35 -0300

    Alterando arquivos para TS

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import { PencilLine } from 'phosphor-react';
+import Avatar from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt=""
+            />
+
+            <div className={styles.profile}>
+                <Avatar
+                    src="https://github.com/endreumrb.png"
+                />
+
+                <strong>Endreu Benites</strong>
+                <span>Front End Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
+
+export default Sidebar;
